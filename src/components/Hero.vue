7b8296e0a9f0c60d2feec62613faa837c1ad15e5
hugo-abdou <moikinge3@gmail.com>
2021-12-25 16:33:59 +0100

    more changes to home page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,7 +7,7 @@
 			class="absolute h-full w-full object-cover object-top"
 		/>
 		<!-- overlay -->
-		<div class="absolute bg-gradient-to-r from-[#000]/70 to-[#000]/20 inset-0"></div>
+		<div class="absolute bg-gradient-to-r from-[#000]/70 via-[#000]/70 to-[#000]/20 inset-0"></div>
 
 		<div class="relative z-10 container mx-auto p-9">
 			<!-- navigation -->
@@ -429,66 +429,57 @@
 			class="absolute h-full w-full object-cover object-top"
 		/>
 		<!-- overlay -->
-		<div class="absolute bg-gray-900/90 inset-0"></div>
-		<div class="relative flex flex-wrap justify-around py-10 text-white container mx-auto">
-			<div class="w-auto lg:w-1/4 text-center lg:text-left">
+		<div class="absolute bg-[#000]/90 inset-0"></div>
+		<div class="relative flex flex-wrap py-10 text-white container mx-auto px-9">
+			<div class="w-auto lg:w-1/4 lg:text-left">
 				<span>Contact</span>
 				<ul class="mt-5 space-y-3 w-full">
 					<li class="flex items-center">
 						<img src="../assets/Vector1.png" alt="phone icon" />
 						<div class="ml-4">
-							<span class="block text-sm text-gray-300">Call Anytime</span>
-							<span class="block text-xl font-semibold">+212 666 888 000</span>
+							<span class="block text-sm text-gray-400">Call Anytime</span>
+							<span class="block text-xl font-semibold text-gray-300">+212 666 888 000</span>
 						</div>
 					</li>
 					<li class="flex items-center">
 						<img src="../assets/Vector2.png" alt="phone icon" />
 						<div class="ml-4">
-							<span class="block text-sm text-gray-300">Send Email</span>
-							<span class="block text-xl font-semibold">example@email.com</span>
+							<span class="block text-sm text-gray-400">Send Email</span>
+							<span class="block text-xl font-semibold text-gray-300">example@email.com</span>
 						</div>
 					</li>
 					<li class="flex items-center">
 						<img src="../assets/Vector3.png" alt="phone icon" />
 						<div class="ml-4">
-							<span class="block text-sm text-gray-300">Visit Office</span>
-							<span class="block text-xl font-semibold">80 Broklyn Street</span>
+							<span class="block text-sm text-gray-400">Visit Office</span>
+							<span class="block text-xl font-semibold text-gray-300">80 Broklyn Street</span>
 						</div>
 					</li>
 				</ul>
 			</div>
-			<div class="w-full lg:w-2/4 justify-around flex mt-10 lg:m-0">
-				<div>
-					<span>Links</span>
+			<div class="w-full lg:w-2/4 flex lg:justify-center mt-10 lg:m-0">
+				<div class="w-1/2">
+					<span class="text-gray-300">Links</span>
 					<ul class="mt-5 space-y-3">
-						<li class="block text-sm text-gray-300">Home</li>
-						<li class="block text-sm text-gray-300">About Us</li>
-						<li class="block text-sm text-gray-300">Forum</li>
-						<li class="block text-sm text-gray-300">Challenges</li>
-						<li class="block text-sm text-gray-300">Contact Us</li>
+						<li class="block text-sm text-gray-400">Home</li>
+						<li class="block text-sm text-gray-400">About Us</li>
+						<li class="block text-sm text-gray-400">Forum</li>
+						<li class="block text-sm text-gray-400">Challenges</li>
+						<li class="block text-sm text-gray-400">Contact Us</li>
 					</ul>
 				</div>
 				<div>
-					<span>Challenges</span>
+					<span class="text-gray-300">Challenges</span>
 					<ul class="mt-5 space-y-3">
-						<li class="block text-sm text-gray-300">categorie 1</li>
-						<li class="block text-sm text-gray-300">categorie 2</li>
-						<li class="block text-sm text-gray-300">categorie 3</li>
-						<li class="block text-sm text-gray-300">categorie 4</li>
-						<li class="block text-sm text-gray-300">categorie 5</li>
+						<li class="block text-sm text-gray-400">categorie 1</li>
+						<li class="block text-sm text-gray-400">categorie 2</li>
+						<li class="block text-sm text-gray-400">categorie 3</li>
+						<li class="block text-sm text-gray-400">categorie 4</li>
+						<li class="block text-sm text-gray-400">categorie 5</li>
 					</ul>
 				</div>
 			</div>
-			<!-- logo -->
-
-			<a class="w-full lg:w-1/4 mt-10 lg:m-0" href="/">
-				<span class="sr-only">Digital Self</span>
-				<img src="../assets/Digital Self.png" alt="dijital self logo" />
-			</a>
-		</div>
-		<div class="relative flex justify-between pb-10 text-white container mx-auto">
-			<span>copyright© 2021</span>
-			<div class="flex text-white space-x-3">
+			<div class="flex text-white space-x-3 lg:hidden w-full mt-9">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -537,6 +528,72 @@
 					<path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
 					<line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
 				</svg>
+			</div>
+			<!-- logo -->
+
+			<a class="w-full lg:w-1/4 mt-10 lg:m-0" href="/">
+				<span class="sr-only">Digital Self</span>
+				<img src="../assets/Digital Self.png" alt="dijital self logo" />
+			</a>
+		</div>
+		<div class="relative container mx-auto px-9 text-white text-p-sm font-normal lg:hidden">
+			<p>
+				<b>My company</b> dolor sit amet, consectetur adipiscing elit. A vitae in enim at diam eu vitae.consectetur adipiscing elit. A vitae in enim at diam eu vitae.consectetur adipiscing elit. A vitae in enim at diam eu vitae.consectetur adipiscing elit.
+			</p>
+		</div>
+		<div class="relative container mx-auto mt-14 lg:mt-0 px-9">
+			<div class="flex justify-between py-5 text-white border-t lg:border-none">
+				<span>copyright© 2021</span>
+				<div class="text-white space-x-3 hidden lg:flex">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="feather feather-twitter"
+					>
+						<path
+							d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="feather feather-linkedin"
+					>
+						<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+						<rect x="2" y="9" width="4" height="12" />
+						<circle cx="4" cy="4" r="2" />
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="feather feather-instagram"
+					>
+						<rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
+						<path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+						<line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
+					</svg>
+				</div>
 			</div>
 		</div>
 	</div>
